39acfb25d0d4663c9c518406f768fc044837d203
Julie Judachina <iuliia.saunova@email.cz>
2021-11-09 11:45:09 +0100

    update

---
+++ contacts.js
@@ -0,0 +1,22 @@
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs').promises;
+
+ const contactsPath = ;
+
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
